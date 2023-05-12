e64fe10646458f20dd7c3a386bb4e623f3a55724
Ana Ádila <anaadilacc@gmail.com>
2023-05-11 21:19:04 -0300

    Add do while and switch case

--- produtos/src/Main.java
+++ produtos/src/Main.java
@@ -1,24 +1,59 @@
 import models.Produto;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println("--- PEDIDO DE VENDAS ---");
 
-        System.out.print("Qual a descrição que você deseja dar ao novo produto? ");
-        String descricao = scanner.nextLine();
-        System.out.print("Qual o ID você deseja dar ao novo produto? ");
-        int id = scanner.nextInt();
+        int option;
 
-        Produto novoProduto = new Produto(id, descricao);
-        //novoProduto.setDescricao(descricao);
-        //novoProduto.setId(id);
+        do {
+            System.out.println("1 - Cadastrar produto");
+            System.out.println("0 - Sair");
 
-        System.out.println("Produto criado com sucesso!");
-        System.out.println("----");
-        System.out.println("ID do produto: " + novoProduto.getId());
-        System.out.println("Seu produto: " + novoProduto.getDescricao());
+            Scanner scanner= new Scanner(System.in);
+            
+            System.out.print("Qual operação você deseja realizar? ");
+            option = scanner.nextInt();
 
+            process(option);
+
+        } while(option !=0);
     }
+
+    public static void process(int option) throws Exception {
+        
+        switch (option) {
+            case 1: {
+                Scanner scanner= new Scanner(System.in);
+
+                System.out.print("Qual a descrição que você deseja dar ao novo produto? ");
+                String descricao = scanner.nextLine();
+                System.out.print("Qual o ID você deseja dar ao novo produto? ");
+                int id = scanner.nextInt();
+                System.out.print("Qual o preço do produto? ");
+                double preco = scanner.nextDouble();
+                System.out.print("Qual a data da validade do produto? ");
+                String dataString = scanner.next();
+                Date dataValidade = new SimpleDateFormat("dd/MM/yyyy").parse(dataString);
+        
+                Produto novoProduto = new Produto(id, descricao, preco, dataValidade);
+        
+                System.out.println("Produto criado com sucesso!");
+                System.out.println("--------------------");
+                System.out.println("ID do produto: " + novoProduto.getId());
+                System.out.println("Descrição: " + novoProduto.getDescricao());
+                System.out.println("Preço: " + novoProduto.getPreco());
+                System.out.println("Data de Validade: " + novoProduto.getDataValidade());
+                System.out.println("--------------------");
+            }
+        }
+    }
+        
+
+
 }
--- produtos/src/models/Produto.java
+++ produtos/src/models/Produto.java
@@ -1,13 +1,19 @@
 package models;
 
+import java.util.Date;
+
 public class Produto {
     
     private int id;
     private String descricao;
+    private double preco;
+    private Date dataValidade;
 
-    public Produto(int id, String descricao) {
+    public Produto(int id, String descricao, double preco, Date dataValidade) {
         this.descricao = descricao;
         this.id = id;
+        this.preco = preco;
+        this.dataValidade = dataValidade;
     }
 
     public void setId(int id) {
@@ -24,5 +30,13 @@ public class Produto {
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
     }
 }
