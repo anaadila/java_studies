f65bf0c64724260e36bc9ecb3e48416cdf1e34b7
Ana Ádila <anaadilacc@gmail.com>
2023-05-14 16:18:54 -0300

    TreeMap

--- geral/Colecoes.java
+++ geral/Colecoes.java
@@ -1,11 +1,12 @@
 package geral;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Colecoes {
     
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, String> map = new TreeMap<>();
         map.put("C", "Carro");
         map.put("M", "Moto");
         map.put("B", "Bicicleta");
@@ -16,4 +17,6 @@ public class Colecoes {
 
         System.out.println(map.get("M"));
     }
+
+    /* HashMap tem mais memória e TreeMap é mais perfomático */
 }
